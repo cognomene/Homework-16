0470edd54bce42ff7340b3fa322869368b21120c
cognomene <116336728+cognomene@users.noreply.github.com>
2022-10-21 15:02:34 +0300

    Update CheckboxPage.cs

--- Page/CheckboxPage.cs
+++ Page/CheckboxPage.cs
@@ -7,37 +7,20 @@ namespace NamuDarbai.Page
     public class CheckboxPage : BasePage
     {
         private string pageAddress => "http://demo.seleniumeasy.com/basic-checkbox-demo.html";
-        //private IWebElement inputField => Driver.FindElement(By.Id("user-message"));
-        //private IWebElement firstBlockButton => Driver.FindElement(By.CssSelector("#get-input > button"));
-        //private IWebElement resultElement => Driver.FindElement(By.Id("display"));
-        //private IWebElement inputFieldA => Driver.FindElement(By.Id("sum1"));
-        //private IWebElement inputFieldB => Driver.FindElement(By.Id("sum2"));
-        //private IWebElement getTotalButton => Driver.FindElement(By.CssSelector("#gettotal>button"));
-        //private IWebElement totalResultElement => Driver.FindElement(By.CssSelector("#displayvalue"));
-
-        // FIRST TEST
         private IWebElement oneCheckBox => Driver.FindElement(By.Id("isAgeSelected"));
         private IWebElement resultElement => Driver.FindElement(By.Id("txtAge"));
-
-        // SECOND TEST
         private IWebElement button => Driver.FindElement(By.CssSelector("#check1"));
-
-
-        // THIRD TEST
-        //private IWebElement button => Driver.FindElement(By.CssSelector("#check1")); button is the same as line 23
-
 
         public CheckboxPage(IWebDriver webdriver) : base(webdriver)
         {
         }
+
 
         public void NavigateToPage()
         {
             Driver.Url = pageAddress;
         }
 
-
-        // visi kiti metodai zemiau
 
         public void OneCheckboxTest()
         {
